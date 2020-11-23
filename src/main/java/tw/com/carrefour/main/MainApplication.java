04b526842f67f6cfb817fe0d310896a251d39f01
KK <kktry57@gmail.com>
2020-11-23 15:53:21 +0800

    使用別人的demo lib版本

--- src/main/java/tw/com/carrefour/main/MainApplication.java
+++ src/main/java/tw/com/carrefour/main/MainApplication.java
@@ -1,18 +1,20 @@
 package tw.com.carrefour.main;
 
-import org.springframework.boot.SpringApplication;
+
+import org.apache.maven.plugin.MojoExecutionException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import tw.com.carrefour.utilities.Bean.HelloBean;
+// import tw.com.carrefour.utilities.Bean.WorldBean;
+import com.github.liufarui.*;
 
 @SpringBootApplication
 public class MainApplication {
 
-	public static void main(String[] args) {
-		
-		HelloBean h = new HelloBean();
-		System.out.println(h.hello());
-		
+	public static void main(String[] args) throws MojoExecutionException {
+		MyMojo m = new MyMojo();
+		m.execute();
+		// WorldBean h = new WorldBean();
+		// System.out.println(h.world());	
 		
 	}
 
